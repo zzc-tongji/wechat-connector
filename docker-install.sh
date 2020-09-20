a70bca4f24056d21e3b63520c4e14e86a3110b62
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2020-09-19 20:56:43 -0400

    commit

--- docker-install.sh
+++ docker-install.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 if [ $(cat /etc/os-release | grep -ci "debian\|ubuntu") -gt 0 ]; then
-  apt-get update
-  && apt-get install -y curl
-  && apt-get install -y gcc
-  && apt-get install -y make
-  && apt-get install -y --no-install-recommends ./google-chrome-unstable_85.0.4173.0-1_amd64.deb
-  && mkdir runtime/
-  && yarn install --production
-  && yarn start-install
-  && cd src/docker-start/
-  && ./compile.sh
+  apt-get update \
+  && apt-get install -y curl \
+  && apt-get install -y gcc \
+  && apt-get install -y make \
+  && apt-get install -y --no-install-recommends ./google-chrome-unstable_85.0.4173.0-1_amd64.deb \
+  && mkdir runtime/ \
+  && yarn install --production \
+  && yarn start-install \
+  && cd src/docker-start/ \
+  && gcc -include whereami.h -c whereami.c \
+  && gcc -include whereami.h -c main.c \
+  && gcc whereami.o main.o -o docker-start \
+  && rm -f whereami.o \
+  && rm -f main.o \
+  && mv docker-start ../../runtime/ \
   && cd ../../
 else
   echo "[Error] unsupported linux distribution"
