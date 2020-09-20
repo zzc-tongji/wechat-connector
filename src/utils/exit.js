28df12199f55d60a4ca418aa272f535e35de436a
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2020-09-19 21:59:37 -0400

    commit

--- src/utils/exit.js
+++ src/utils/exit.js
@@ -3,8 +3,8 @@ import { sep } from 'path';
 
 import { global } from './global';
 
-const directoryPath = `${__dirname}${sep}..${sep}..${sep}runtime-private${sep}`;
-const exitScriptPath = `${directoryPath}docker-stop.sh`;
+const directoryPath = `${__dirname}${sep}..${sep}..${sep}runtime-private`;
+const exitScriptPath = `${directoryPath}${sep}docker-stop.sh`;
 
 const generateExitScript = () => {
   const content = `#!/bin/sh\n\ncurl --silent --header "content-type:application/json;charset=UTF-8" --request POST --data '{"rpcToken":"${global.setting.http.receiver.rpcToken}"}' http://127.0.0.1:${global.setting.http.receiver.port}/rpc/exit\n`;
