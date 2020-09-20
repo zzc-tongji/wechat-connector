9747757a3905d245e8c7c1d4bd8a67e45c40277e
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2020-09-19 21:44:04 -0400

    commit

--- src/utils/exit.js
+++ src/utils/exit.js
@@ -7,7 +7,7 @@ const directoryPath = `${__dirname}${sep}..${sep}..${sep}runtime-private${sep}`;
 const exitScriptPath = `${directoryPath}docker-stop.sh`;
 
 const generateExitScript = () => {
-  const content = `#!/bin/sh\n\ncurl --header "content-type:application/json;charset=UTF-8" --request POST --data '{"rpcToken":"${global.setting.http.receiver.rpcToken}"}' http://127.0.0.1:${global.setting.http.receiver.port}/rpc/exit\n`;
+  const content = `#!/bin/sh\n\ncurl --silent --header "content-type:application/json;charset=UTF-8" --request POST --data '{"rpcToken":"${global.setting.http.receiver.rpcToken}"}' http://127.0.0.1:${global.setting.http.receiver.port}/rpc/exit\n`;
   if (!fs.existsSync(directoryPath)) {
     fs.mkdirSync(directoryPath);
   }
